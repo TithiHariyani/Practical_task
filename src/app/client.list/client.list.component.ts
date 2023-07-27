b986e02f6ecd75154487e9ccc70d1382f05e1d54
Tithi <tithihariyani28@gmail.com>
2023-07-27 09:46:15 +0530

    Updates

--- src/app/client.list/client.list.component.ts
+++ src/app/client.list/client.list.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./client.list.component.css']
 })
 export class ClientListComponent {
+  clients:any = [];
+  constructor(private _http:HttpClient){      
+    this.getClientList().subscribe({
+     next:(data)=>{
+      this.clients = data
+    },
+    error:()=>{alert('Error Occured During Retrival Of Clients Data !!!');},
+    complete:()=>{}
+  });
+  }
+  
+  getClientList = ()=>{
+    return this._http.get('http://test.ultim8e.net/api/clients');
+  }
 
+  removeClient = (id:number)=>{
+    console.log('called');
+    
+    let res = this.remove(id).subscribe({next:(data)=>{},error:(err)=>{},complete:()=>{}});
+    console.log(res);
+    
+  }
+
+  remove = (id:number)=>{
+    return this._http.delete(`http://test.ultim8e.net/api/clients/${id}`);
+  }
+
+  editClient = (id:number,data:any)=>{
+    this.edit(id,data).subscribe({next:(data)=>{},error:(err)=>{},complete:()=>{}});
+  }
+
+  edit = (id:number,data:any)=>{
+    return this._http.put(`http://test.ultim8e.net/api/clients/${id}`,data);
+  }
 }
