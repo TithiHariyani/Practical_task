87bd575493c886509e2454aefbe529deca85acf8
Tithi <tithihariyani28@gmail.com>
2023-07-28 09:51:05 +0530

    updates

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -1,13 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthServiceService {
+  // HttpClient: any;
+  // form: any;
+  // router: any;
 
   constructor(private http:HttpClient) { }
   login(data: any): Observable<any>{
+    console.log("Sucsess");
     return this.http.post(`http://test.ultim8e.net/api/auth/login`, data);
   }
+
+
+  onLogin(obj: any) : Observable<any>{
+    return this.http.post('http://test.ultim8e.net/api/auth/login', obj);
+  }
+
+  getClients() : Observable<any> {
+    return this.http.get('http://test.ultim8e.net/api/clients');
+  }
+
+  // constructor(private http: HttpClient) {}
+
+  // login(username: string, password: string){
+  // this.HttpClient.login(this.form.get('username')?.value, this.form.get('password')?.value)
+  // .subscribe((response: any) => {
+  //   console.log(response);
+  //   this.router.navigate(['/client.list']);
+  // });
+  // }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginUser: any[] = [];
+  loginObj = {
+    email: '',
+    password: '',
+  };
+
   formGroup !: FormGroup;
   constructor(private authService: AuthServiceService, private router: Router) {}
   ngOnInit() {
@@ -17,7 +23,13 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin() {
-    this.router.navigate(['/client.list']);
+    // this.router.navigate(['/client.list']);
+    this.authService.onLogin(this.loginObj).subscribe((res: any) => {
+      debugger
+      console.log('res', res);
+      localStorage.setItem('token', res.token);
+      this.router.navigateByUrl('/client-list');
+    })
   }
 
   initForm():any {
